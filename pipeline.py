f114eafd2d7e8159baf096a3487e5d9b62901e75
shlomihod <6306135+shlomihod@users.noreply.github.com>
2024-07-22 19:13:34 -0400

    add tradeoff results

--- pipeline.py
+++ pipeline.py
@@ -32,7 +32,7 @@ except ImportError:
 
 # https://pages.nist.gov/privacy_collaborative_research_cycle/pages/participate.html
 COL_SUBSETS = {
-    "demographic": ["SEX", "MSP", "RAC1P", "OWN_RENT", "PINCP_DECILE", "EDU", "AGEP", "HOUSING_TYPE", "DVET", "DEYE"]
+    "demographic": ["SEX", "MSP", "RAC1P", "OWN_RENT", "PINCP_DECILE", "EDU"]  #, "AGEP", "HOUSING_TYPE", "DVET", "DEYE"]
 }
 
 BASELINE_DATASETS = ["baseline_domain", "baseline_univariate"]
