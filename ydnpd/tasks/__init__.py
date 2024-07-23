a2e887ca139dac512ed8d3af0ed3f8fb5d7d1f2e
shlomihod <6306135+shlomihod@users.noreply.github.com>
2024-07-23 14:16:37 -0400

    add tradeoff task

--- ydnpd/tasks/__init__.py
+++ ydnpd/tasks/__init__.py
@@ -1,2 +1,3 @@
 from ydnpd.tasks.base import DPTask
 from ydnpd.tasks.hparams import HyperParamSearchTask
+from ydnpd.tasks.tradeoff import PrivacyUtilityTradeoffTask
--- ydnpd/tasks/tradeoff.py
+++ ydnpd/tasks/tradeoff.py
@@ -1,23 +1,63 @@
-from ydnpd.tasks.hparams import HyperParamSearchTask
+import pandas as pd
+import seaborn as sns
 
+from ydnpd.tasks.hparams import HyperParamSearchTask
 
 
 class PrivacyUtilityTradeoffTask:
 
-    def __init__(self):
-        pass
+    def __init__(self, hparams_task: HyperParamSearchTask):
+        self.hparams_task = hparams_task
 
     def execute(self):
         raise RuntimeError("Use HyperParamSearchTask to produce results")
 
-    def evaluate(self, *, hparams_dev_results, hparams_test_results, **kwargs) -> dict[str, float]:
-        results_df = HyperParamSearchTask.combine_results(dev_results=hparams_dev_results,
-                                                            test_results=hparams_test_results)
-        
-        pass
-        # pick the best configuration per epsilon in dev
+    def evaluate(self, hparams_results, *, dev_name, test_name, metric=None, **kwargs):
 
-    def plot(self, *, dev_results, test_results, **kwargs):
-        pass
+        if metric is None:
+            metric = self.hparams_task.METRIC_DEFAULT
 
+        dev_results = hparams_results[dev_name]
+        test_results = hparams_results[test_name]
 
+        dev_results = hparams_results[dev_name]
+        test_results = hparams_results[test_name]
+
+        results_df = HyperParamSearchTask.combine_results(dev_results=dev_results,
+                                                          test_results=test_results)
+        evaluation_df = (results_df
+                         .groupby("epsilon")
+                         .apply(lambda g:
+                                pd.Series({f"{take}_{by}": g.at[g[f"evaluation_{by}"].str[metric].idxmin(),
+                                                                f"evaluation_{take}"][metric]
+                                                                for take, by in [("dev", "dev"),
+                                                                                 ("test", "dev"),
+                                                                                 ("test", "test")]}))
+                         )
+
+        return evaluation_df
+
+    def plot(self, hparams_results, *, dev_name, test_name, metric=None, **kwargs):
+        if metric is None:
+            metric = self.hparams_task.METRIC_DEFAULT
+
+        evaluation_df = self.evaluate(hparams_results,
+                                      dev_name=dev_name,
+                                      test_name=test_name)
+
+        melted_df = (evaluation_df
+                     .reset_index()
+                     .melt(id_vars=["epsilon"],
+                           var_name="condition",
+                           value_name="value")
+                     )
+
+        g = sns.relplot(data=melted_df,
+                        x="epsilon", y="value", hue="condition",
+                        kind="line")
+
+        g.figure.suptitle(f"P-U Tradeoff of Dev {dev_name} and Test {test_name}")
+        g.set(xticks=melted_df["epsilon"].unique())
+        g.set_axis_labels("ε", metric)
+
+        return g
