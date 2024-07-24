d7df86c16e649db04fddaa08bfde4e6d36e2b823
shlomihod <6306135+shlomihod@users.noreply.github.com>
2024-07-24 18:57:29 -0400

    drop hparam dims info

--- ydnpd/__init__.py
+++ ydnpd/__init__.py
@@ -1,4 +1,4 @@
 from ydnpd.dataset import load_dataset
 from ydnpd.tasks import HyperParamSearchTask, PrivacyUtilityTradeoffTask
-from ydnpd.synthesis import generate_synthetic_data, HPARAMS_BASE, SYNTHESIZERS
+from ydnpd.synthesis import generate_synthetic_data, SYNTHESIZERS
 from ydnpd.utils import suppress_output
