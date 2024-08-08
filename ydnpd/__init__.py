57c34e2576e6d414317c6913e38c594856dc04c8
shlomihod <6306135+shlomihod@users.noreply.github.com>
2024-08-08 12:30:44 -0400

    fix imports and style

--- ydnpd/__init__.py
+++ ydnpd/__init__.py
@@ -1,8 +1,8 @@
 from ydnpd.dataset import load_dataset
 from ydnpd.evaluation import evaluate_two, EVALUATION_METRICS
-from ydnpd.experiment import Experiment
+from ydnpd.experiment import Experiments
 from ydnpd.tasks import UtilityTask
 from ydnpd.synthesis import generate_synthetic_data, SYNTHESIZERS
-from ydnpd.ray import span_hparam_tasks, span_hparam_ray_tasks
+from ydnpd.ray import span_utility_tasks, span_utility_ray_tasks
 from ydnpd.utils import suppress_output
 from ydnpd import config
