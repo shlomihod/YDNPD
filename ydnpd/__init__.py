bec298f07ca36b8ee1c29d091a7eeef71834d415
shlomihod <6306135+shlomihod@users.noreply.github.com>
2025-01-14 09:00:02 +0100

    updatea dataset plot

--- ydnpd/__init__.py
+++ ydnpd/__init__.py
@@ -1,4 +1,5 @@
 from ydnpd.harness import (span_utility_tasks, span_utility_ray_tasks,
                            UtilityTask, Experiments,
                            ALL_EXPERIMENTS, EVALUATION_METRICS)
-from ydnpd.datasets.loader import load_dataset
+from ydnpd.datasets import load_dataset, plot_distribution_distances
+
