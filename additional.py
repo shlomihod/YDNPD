e0e56d7cf9dadea145901893bf2272aa14885897
shlomihod <6306135+shlomihod@users.noreply.github.com>
2025-03-12 09:50:42 +0100

    drop sdscm

--- additional.py
+++ additional.py
@@ -7,6 +7,7 @@ ADDITIONAL_EXPERIMENTS = {
     [
         (f"{experiment_name}/{path.stem}", ADDITIONAL_PATH)
         for path in Path(f"{ADDITIONAL_PATH}/{experiment_name}").glob("*.csv")
+        if "scm" not in str(path)
         ]
         for experiment_name in ["acs", "edad", "we"]
 }
